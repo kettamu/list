5a18588eea2c5a5b27063b3543a84862d06c2044
mityagov_vi <windead2@gmail.com>
2015-11-12 10:26:15 +0300

    1

--- views.py
+++ views.py
@@ -24,45 +24,7 @@ class detailtasklist(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-"""
-@api_view(['GET', 'POST'])
-def snippet_list(request):
-    if request.method == 'GET':
-        if not request.user.is_superuser:
-            snippet = Snippet.objects.all().filter(username=request.user)
-        else:
-            snippet = Snippet.objects.all()
-        serializer = SnippetSerializer(snippet, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
-    try:
-        snippet = Snippet.objects.get(id=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return Response(status=status.HTTP_410_GONE)
-
-"""
 @api_view(['GET'])
 def snippet_search(request, search):
     if request.method == 'GET':
